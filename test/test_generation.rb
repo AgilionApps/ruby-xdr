84c2fed6c3b6ee467c60e8d009417cd765464670
Matthew Booth <mbooth@redhat.com>
2011-01-04 09:34:31 +0000

    Give test_generator.rb a unique test class

--- test/test_generation.rb
+++ test/test_generation.rb
@@ -4,22 +4,22 @@ require 'stringio'
 require 'xdr'
 require 'xdr/parser'
 
-class ParserTest < Test::Unit::TestCase
+class GeneratorTest < Test::Unit::TestCase
     def test_enum
         p = XDR::Parser.new(StringIO.new("enum a { A, B };"))
-        p.load('ParserTest::Test_enum0')
+        p.load('GeneratorTest::Test_enum0')
 
-        assert_equal(ParserTest::Test_enum0::A::A, 0)
-        assert_equal(ParserTest::Test_enum0::A::B, 1)
+        assert_equal(GeneratorTest::Test_enum0::A::A, 0)
+        assert_equal(GeneratorTest::Test_enum0::A::B, 1)
 
         assert_nothing_raised do
-            ParserTest::Test_enum0::A.new()
-            ParserTest::Test_enum0::A.new(0)
-            ParserTest::Test_enum0::A.new(1)
+            GeneratorTest::Test_enum0::A.new()
+            GeneratorTest::Test_enum0::A.new(0)
+            GeneratorTest::Test_enum0::A.new(1)
         end
 
         assert_raise(ArgumentError) do
-            ParserTest::Test_enum0::A.new(2)
+            GeneratorTest::Test_enum0::A.new(2)
         end
 
         p = XDR::Parser.new(StringIO.new(<<TEST))
@@ -33,14 +33,14 @@ enum a {
 };
 const FOO = B;
 TEST
-        p.load('ParserTest::Test_enum1')
+        p.load('GeneratorTest::Test_enum1')
 
-        assert_equal(ParserTest::Test_enum1::A::A, 0);
-        assert_equal(ParserTest::Test_enum1::A::B, 1);
-        assert_equal(ParserTest::Test_enum1::A::C, 5);
-        assert_equal(ParserTest::Test_enum1::A::D, 6);
-        assert_equal(ParserTest::Test_enum1::A::E, 1);
-        assert_equal(ParserTest::Test_enum1::A::F, 2);
+        assert_equal(GeneratorTest::Test_enum1::A::A, 0);
+        assert_equal(GeneratorTest::Test_enum1::A::B, 1);
+        assert_equal(GeneratorTest::Test_enum1::A::C, 5);
+        assert_equal(GeneratorTest::Test_enum1::A::D, 6);
+        assert_equal(GeneratorTest::Test_enum1::A::E, 1);
+        assert_equal(GeneratorTest::Test_enum1::A::F, 2);
 
         p = XDR::Parser.new(StringIO.new(<<TEST))
 enum a {
@@ -52,7 +52,7 @@ const FOO = C;
 TEST
 
         assert_raise(XDR::ConstantDefinitionLoop) do
-            p.load('ParserTest::Test_enum2')
+            p.load('GeneratorTest::Test_enum2')
         end
     end
 
@@ -62,10 +62,10 @@ const b = a;
 const a = 1;
 TEST
 
-        p.load('ParserTest::Test_constant')
+        p.load('GeneratorTest::Test_constant')
 
-        assert_equal(ParserTest::Test_constant::A, 1)
-        assert_equal(ParserTest::Test_constant::B, 1)
+        assert_equal(GeneratorTest::Test_constant::A, 1)
+        assert_equal(GeneratorTest::Test_constant::B, 1)
     end
 
     def test_constant_loop
@@ -76,7 +76,7 @@ const c = a;
 TEST
 
         assert_raise(XDR::ConstantDefinitionLoop) do
-            p.load('ParserTest::Test_constant_loop')
+            p.load('GeneratorTest::Test_constant_loop')
         end
     end
 
@@ -87,10 +87,10 @@ typedef a b;
 TEST
 
         assert_nothing_raised do
-            p.load('ParserTest::Test_typedef0')
+            p.load('GeneratorTest::Test_typedef0')
 
-            assert_equal(ParserTest::Test_typedef0::A::A, 0);
-            assert_equal(ParserTest::Test_typedef0::B::A, 0);
+            assert_equal(GeneratorTest::Test_typedef0::A::A, 0);
+            assert_equal(GeneratorTest::Test_typedef0::B::A, 0);
         end
 
         p = XDR::Parser.new(StringIO.new(<<TEST))
@@ -100,7 +100,7 @@ typedef c a;
 TEST
 
         assert_raise(XDR::TypeDefinitionLoop) do
-            p.load('ParserTest::Test_typedef1')
+            p.load('GeneratorTest::Test_typedef1')
         end
     end
 end
